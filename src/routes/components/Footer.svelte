a3204a982ffe8a3325bfc682873af7ffcaefda07
Araad <100861501+araad-wallit@users.noreply.github.com>
2023-02-25 21:14:31 +0330

    feat: [#1] move footer and header files

---
+++ src/routes/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer class="[ pbe-16 pbs-8 text-gray-200 tracking-wide ]">
+	<div class="[ flex flex-col items-center gap-2 ] [ container select-none ]">
+		<strong class="[ text-green-500 text-lg ]"><i class="oji">💸</i> PAYTO Money</strong>
+
+		<p>Licensed under the <strong class="[ text-gray-200 ]">CORE</strong> license.</p>
+
+		<p class="[ flex flex-col items-center gap-2 ]">
+			<span>
+				Source code is located at
+				<a
+					class="[ transition-all duration-200 ] [ visited:text-gray-200 hover:text-gray-300 ]"
+					href="https://github.com/cryptohub-digital/Payto"
+				>
+					GitHub
+				</a>.
+			</span>
+			<small class="[ text-sm ]">feel free to contribute</small>
+		</p>
+	</div>
+</footer>
---
+++ src/routes/components/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Icon } from '$lib/icons';
+</script>
+
+<header class="[ grid place-content-center ] [ pbs-16 pbe-8 ]">
+	<!-- <img src="/icons/favicon-32x32.png" alt="Payto scheme" width="32" height="32"> -->
+
+	<div class="[ flex flex-col items-stretch ] [ container select-none ] [ lg:items-center ]">
+		<h1
+			class="[ flex flex-col items-start gap-2 ] [ text-green-500 text-5xl tracking-wide ] [ lg:text-6xl lg:flex-row ]"
+		>
+			<span class="[ font-bold ]">PAYTO</span>
+			<span>Integrations</span>
+		</h1>
+
+		<a
+			class="[ inline-flex items-center gap-2 ] [ group font-semibold text-gray-200 transition-all duration-200 ] [ visited:text-gray-200 hover:text-gray-300 ]"
+			href="https://Payto.money/"
+		>
+			<span>visit</span>
+			<span class="[ inline-flex items-end gap-2 ]">
+				<span>PAYTO Money</span>
+				<Icon.ArrowRight
+					class="[ bs-5 is-5 transition-all duration-200 ] [ group-hover:translate-x-1 ]"
+				/>
+			</span>
+		</a>
+	</div>
+</header>
