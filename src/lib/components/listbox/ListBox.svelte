9d05c72cce6d5451055417d8663cdae4a9371e5b
Araad <100861501+araad-wallit@users.noreply.github.com>
2023-02-25 21:11:41 +0330

    feat: [#1] implement list-box component

---
+++ src/lib/components/listbox/ListBox.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { join } from '$lib/helpers/join.helper';
+	import { Listbox } from '@rgossiaux/svelte-headlessui';
+
+	export let value: string | undefined;
+</script>
+
+<Listbox class={join('[ relative mt-1 ]', $$props.class)} {value} on:change>
+	<slot />
+</Listbox>
---
+++ src/lib/components/listbox/index.ts
@@ -0,0 +1,4 @@
+export { default as ListBox } from './ListBox.svelte';
+export { default as ListBoxButton } from './ListBoxButton.svelte';
+export { default as ListBoxMenu } from './ListBoxMenu.svelte';
+export { default as ListBoxOption } from './ListBoxOption.svelte';
