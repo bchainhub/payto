1c2b5d054e48ca44a701c9dc5816292506e44c5a
Araad <100861501+araad-wallit@users.noreply.github.com>
2023-02-25 21:12:30 +0330

    feat: [#1] implement page component

---
+++ src/lib/components/page/Page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Payto:// — The Payto money URI generator</title>
+	<meta name="description" content="The Payto money URI scheme generator." />
+	<meta property="twitter:title" content="Payto:// — The Payto money URI generator" />
+	<meta property="twitter:description" content="The Payto URI scheme generator." />
+	<meta property="twitter:image" content="https://Payto.money/icons/android-chrome-512x512.png" />
+	<meta property="og:title" content="Payto:// — The Payto money URI generator" />
+	<meta property="og:description" content="The Payto money URI scheme generator." />
+	<meta property="og:image" content="https://Payto.money/icons/android-chrome-512x512.png" />
+</svelte:head>
+
+<div
+	class="[ flex flex-col items-stretch justify-between gap-8 ] [ container plb-4 mlb-4 ] [ lg:flex-row lg:min-bs-[62rem] lg:plb-8 lg:mlb-8 ]"
+>
+	<slot />
+</div>
