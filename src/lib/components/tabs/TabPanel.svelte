a59f102e8f82c2cc30d86d91c0869ac8e9d75f13
Araad <100861501+araad-wallit@users.noreply.github.com>
2023-02-25 21:12:52 +0330

    feat: [#1] implement tab component

---
+++ src/lib/components/tabs/TabPanel.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { join } from '$lib/helpers/join.helper';
+	import { TabPanel } from '@rgossiaux/svelte-headlessui';
+</script>
+
+<TabPanel
+	class={join(
+		'[ outline-none ]',
+		'[ focus:outline-none focus-visible:ring-1 focus-visible:ring-opacity-75 focus-visible:ring-green-800 focus-visible:ring-offset-green-700 focus-visible:ring-offset-2 ]'
+	)}
+>
+	<slot />
+</TabPanel>
