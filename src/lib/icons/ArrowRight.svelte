999a994cd994ce71ab4d7f4286009441f5c14611
Araad <100861501+araad-wallit@users.noreply.github.com>
2023-02-25 21:13:52 +0330

    feat: [#1] add svg icons

---
+++ src/lib/icons/ArrowRight.svelte
@@ -0,0 +1,15 @@
+<svg
+	class={$$props.class}
+	fill="none"
+	stroke="currentColor"
+	stroke-width="1.5"
+	viewBox="0 0 24 24"
+	xmlns="http://www.w3.org/2000/svg"
+	aria-hidden="true"
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+	/>
+</svg>
---
+++ src/lib/icons/index.ts
@@ -0,0 +1,7 @@
+import Convert from './Convert.svelte';
+import ArrowRight from './ArrowRight.svelte';
+
+export const Icon = {
+	Convert,
+	ArrowRight
+};
