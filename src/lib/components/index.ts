fad0301dbb433f62c56351832e2306d1e85870f8
Araad <100861501+araad-wallit@users.noreply.github.com>
2023-02-25 21:13:24 +0330

    feat: [#1] export all components

---
+++ src/lib/components/index.ts
@@ -0,0 +1,11 @@
+export { Page } from './page';
+export { Box, BoxTitle, BoxContent } from './box';
+export { Tab, TabGroup, TabList, TabPanel, TabPanels } from './tabs';
+export { ListBox, ListBoxButton, ListBoxMenu, ListBoxOption } from './listbox';
+export {
+	FieldGroup,
+	FieldGroupLabel,
+	FieldGroupText,
+	FieldGroupNumber,
+	FieldGroupAppendix
+} from './fieldgroup';
