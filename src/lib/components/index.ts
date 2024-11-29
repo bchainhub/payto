cd402857c859a0c74ea3aa2dea576dd8085f2829
Rastislav <rastislav@onion.email>
2024-11-29 16:51:48 +0100

    Refactor components

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,11 +1,5 @@
-export { DesignContent } from './designContent';
-export { Page } from './page';
 export { Box, BoxTitle, BoxContent } from './box';
-export { Row } from './row';
-export { Listbox } from './listBox';
-export { Tabs } from './tabs';
-export { toast } from './toast';
-export { default as Toast } from './toast/toast.svelte';
+export { DesignContent } from './design-content';
 export {
 	FieldGroup,
 	FieldGroupColorPicker,
@@ -16,5 +10,10 @@ export {
 	FieldGroupDateTime,
 	FieldGroupAppendix,
 	FieldGroupRadioWithNumber
-} from './fieldgroup';
-export { WalletCard } from './walletCard';
+} from './field-group';
+export { Listbox } from './list-box';
+export { Page } from './page';
+export { Row } from './row';
+export { Tabs } from './tabs';
+export { Toast, toast } from './toast';
+export { WalletCard } from './wallet-card';
